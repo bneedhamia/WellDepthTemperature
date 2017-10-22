dd7a1348d9b6927173ed2245e11f306a7206895f
Bradford Needham <bneedhamia@gmail.com>
2017-10-22 12:47:06 -0700

    Messed about a lot. Added WiFi error checks and timeout.

--- WellDepthTemperature.ino
+++ WellDepthTemperature.ino
@@ -1,7 +1,10 @@
 /*
-   Sparkfun ESP8266 Thing Dev board Sketch
+   Arduino Sketch for a Sparkfun ESP8266 Thing Dev board
    to estimate the height of the water in our well tank
    and send this estimate to a web server.
+   Water height is estimated using temperature differences read
+   from a vertical line of MAX31820 temperature sensors
+   glued to the tank exterior.
 
    Copyright (c) 2017 Bradford Needham
    { @bneedhamia , https://www.needhamia.com }
@@ -11,27 +14,29 @@
 */
 //345678901234567890123456789312345678941234567895123456789612345678971234567898
 
+/*
+   Flags to change the program behavior:
+   PRINT_1_WIRE_ADDRESSES = uncomment this line to only
+     print all the temperature sensors' 1-wire addresses,
+     so you can copy them into the sensorAddress[] initializer.
+     Comment this line out for normal Sketch behavior.
+*/
+//#define PRINT_1_WIRE_ADDRESSES true
+
 #include <ESP8266WiFi.h> // Defines WiFi, the WiFi controller object
 #include <ESP8266HTTPClient.h>
 #include <EEPROM.h>   // NOTE: ESP8266 EEPROM library differs from Arduino's.
 #include <OneWire.h>  // https://github.com/PaulStoffregen/OneWire
-#include <DallasTemperature.h> // from https://github.com/milesburton/Arduino-Temperature-Control-Library
+#include <DallasTemperature.h> // https://github.com/milesburton/Arduino-Temperature-Control-Library
 
 /*
- * Flags to change the program behavior:
- * PRINT_ADDRESSES = uncomment this line to
- *   just print all the temperature sensors' 1-wire addresses,
- *   so you can copy them into the sensorAddress[] initializer.
- *   Comment this line out for normal program behavior.
- */
-//#define PRINT_ADDRESSES true
+   Hardware information:
 
-/*
    PIN_LED_L = LOW to light the on-board LED.
      The _L suffix indicates that the pin is Active-Low.
      Active-Low because that's how the ESP8266 Thing Dev board
      on-board LED is wired.
-   PIN_ONE_WIRE = the 1-Wire bus that all the temperature sensors
+   PIN_ONE_WIRE = the 1-Wire bus data line that all the temperature sensors
      are connected to.
 
    NUM_SENSORS = number of temperature sensors wired to the 1-Wire bus.
@@ -50,8 +55,23 @@ const int PIN_ONE_WIRE = 4;
 
 const int NUM_SENSORS = 12;
 
-// Time (milliseconds) per attempt to read and report the temperatures.
-const long MS_PER_TEMPERATURE_REQUEST = 1000L * 10;
+/*
+ * Time (milliseconds) per attempt to read and report the temperatures.
+ * Set this to, say 100L * 5 for temperature testing (e.g., to see which
+ * sensor is which by heating one and seeing which sensor temperature changes).
+ * Set this to over 5 minutes (1000L * 60L * 5) for normal operation,
+ * so you don't overwhelm the server.
+ */
+const long MS_PER_TEMPERATURE_REQUEST = 1000L * 5;
+
+/*
+   The resolution (number of bits) to set the temperature sensors to.
+   12 is the maximum; 9 is the minimum.
+   Higher resolution takes longer (see the Max31820 datasheet for details)
+   but is more precise.
+*/
+const uint8_t MAX31820_RESOLUTION_BITS = 12;
+
 /*
    The EEPROM layout, starting at START_ADDRESS, is:
    String[EEPROM_WIFI_SSID_INDEX] = WiFi SSID. A null-terminated string
@@ -71,7 +91,7 @@ const int EEPROM_WIFI_PASS_INDEX = 1;
 /*
    The states of the state machine that loop() runs.
 
-   state = the current state of the machine. An STATE_* value.
+   state = the current state of the machine. A STATE_* value.
    stateBegunMs = if needed, the time (millis() we entered this state.
 
    STATE_ERROR = an unrecoverable error has occurred. We stop.
@@ -94,11 +114,16 @@ unsigned long stateBegunMs = 0L;
    httpGet = the object that controls the WiFi stack.
    pHttpStream = if non-null, the stream of data from our Http Get request
    wire = The 1-Wire interface manager.
+
+   NOTE: because the ESP8266 WiFi operates in the background,
+     loop() must not take a long time.
+     In particular, never (even in setup()) call any delay() greater than
+     a few hundred milliseconds.
 */
 HTTPClient httpGet;
 WiFiClient *pHttpStream = 0;
 OneWire wire(PIN_ONE_WIRE);
-DallasTemperature sensors(&wire);
+DallasTemperature wireDevices(&wire);
 
 /*
    WiFi access point parameters.
@@ -108,18 +133,21 @@ DallasTemperature sensors(&wire);
 */
 char *wifiSsid;
 char *wifiPassword;
+const long WIFI_TIMEOUT_MS = 1000L * 60; // eventually move to EEPROM
 
 /*
    sensorAddress[] = the 1-wire address of each sensor on the 1-wire bus,
-   in order of distance from the controller. Note: this order is not the
-   order the sensors are returned by a search().
+   in order of distance from the controller ([0] = closest).
+   Note: this order is not the order the sensors are returned by a search().
 
    I created this table by:
-   1) Setting PRINT_ADDRESSES (above) and copying and pasting the output.
-   2) Commenting out PRINT_ADDRESSES (above)
-   3) In turn, heating (or cooling) one sensor and seeing which position
-     that sensor is in.
-   3) rearranging the rows here to order by distance from the processor.
+   1) Uncommenting PRINT_1_WIRE_ADDRESSES (above)
+   2) Running the Sketch, and copying and pasting the output here.
+   3) Commenting out PRINT_1_WIRE_ADDRESSES (above)
+   4) Starting the Sketch.
+   5) In turn, heating (or cooling) one sensor and seeing which position
+     that sensor is in, based on the printed temperatures.
+   6) rearranging the rows here to order by distance from the processor.
 */
 DeviceAddress sensorAddress[NUM_SENSORS] = {
   {0x28, 0xC6, 0x8A, 0xA8, 0x7, 0x0, 0x0, 0x8A},
@@ -136,62 +164,63 @@ DeviceAddress sensorAddress[NUM_SENSORS] = {
   {0x28, 0xAB, 0xA8, 0xA8, 0x7, 0x0, 0x0, 0x78}
 };
 
-/*
-   The time (millis()) that we most recently issued
-   a read command to the temperature sensors.
-*/
-unsigned long lastReadTimeMs;
-
 // Called once automatically on Reset.
 void setup() {
   Serial.begin(9600);
+  delay(100); // give the Serial port time to power up.
   Serial.println(F("Reset."));
 
-  // Set up all our pins.
+  // Set up our pins.
 
   pinMode(PIN_LED_L, OUTPUT);
   digitalWrite(PIN_LED_L, HIGH); // ESP8266 Thing Dev LED is Active Low
+  // PIN_ONE_WIRE is managed by the OneWire library.
+
+  wireDevices.begin();  // searches the 1-wire bus for devices.
+
+#if defined(PRINT_1_WIRE_ADDRESSES)
+  // Report the device addresses, in no particular order, and quit.
+  print1WireAddresses();
+
+  state = STATE_ERROR;
+  return;
+#endif
 
   // read the wifi credentials from EEPROM, if they're there.
   wifiSsid = readEEPROMString(START_ADDRESS, 0);
   wifiPassword = readEEPROMString(START_ADDRESS, 1);
   if (wifiSsid == 0 || wifiPassword == 0) {
     Serial.println(F("EEPROM not initialized."));
-    //TODO mark a state that loop should not execute.
+
+    state = STATE_ERROR;
     return;
   }
 
-  // Do the one-time WiFi setup:
+  // Do the one-time WiFi setup and connection
   WiFi.mode(WIFI_STA);    // Station (Client), not soft AP or dual mode.
   WiFi.setAutoConnect(false); // don't connect until I say
   WiFi.setAutoReconnect(true); // if the connection ever drops, reconnect.
-
-  connectToAccessPoint();
-
-  sensors.begin();  // searches the 1-wire bus for devices.
-
-#if defined(PRINT_ADDRESSES)
-  // Report the device addresses, in no particular order, and quit.
-  print1WireAddresses();
-  state = STATE_ERROR;
-  return;
-#endif
+  if (!connectToAccessPoint(wifiSsid, wifiPassword, WIFI_TIMEOUT_MS)) {
+    state = STATE_ERROR;
+    return;
+  }
 
   /*
      Set up the temperature sensors:
-     Set to the highest resolution.
+     Set the digital temperature resolution.
      Set the library to have requestTemperatures() return immediately
-       rather than waiting the (long) conversion time.
+       rather than waiting the (too long for our WiFi) conversion time.
   */
-  sensors.setResolution(12);
-  sensors.setWaitForConversion(false);
+  wireDevices.setResolution(MAX31820_RESOLUTION_BITS);
+  wireDevices.setWaitForConversion(false);
 
   // Start the first temperature reading.
-  sensors.requestTemperatures();
+  wireDevices.requestTemperatures();
   state = STATE_WAITING_FOR_TEMPERATURES;
   stateBegunMs = millis();
 }
 
+// Called repeatedly, automatically.
 void loop() {
   long nowMs;
   boolean succeeded; // a general flag for remembering a failure.
@@ -209,26 +238,27 @@ void loop() {
       break;
 
     case STATE_WAITING_FOR_TEMPERATURES:
-      if ((nowMs - stateBegunMs) < sensors.millisToWaitForConversion(12)) {
+      if ((long) (nowMs - stateBegunMs)
+          < wireDevices.millisToWaitForConversion(MAX31820_RESOLUTION_BITS)) {
         return; // wait more.
       }
 
       // The temperatures are ready. Read them.
       succeeded = true;
       for (int i = 0; i < NUM_SENSORS; ++i) {
-        float tempC = sensors.getTempC(sensorAddress[i]);
+        float tempC = wireDevices.getTempC(sensorAddress[i]);
         if (tempC <= DEVICE_DISCONNECTED_C) {
           succeeded = false;
         }
-        
+
         // for now, just report the temperature
         if (i != 0) {
           Serial.print(", ");
         }
-        Serial.print(tempC, 1); // output 20.5 vs. 20.541
+        Serial.print(tempC, 1); // output 20.5 vs. 20.54
       }
       Serial.println();
-      
+
       if (succeeded) {
         // report the temperatures to the web server.
         // Maybe always report them, converting the error value to a better one.
@@ -237,20 +267,20 @@ void loop() {
       // Wait until it's time to request the temperatures again.
       state = STATE_WAITING_FOR_NEXT_READ;
       stateBegunMs = nowMs;
-      
+
       break;
 
     case STATE_WAITING_FOR_NEXT_READ:
-      if ((nowMs - stateBegunMs) < MS_PER_TEMPERATURE_REQUEST) {
+      if ((long) (nowMs - stateBegunMs) < MS_PER_TEMPERATURE_REQUEST) {
         return; // wait more.
       }
 
       // Request the sensors to read their temperatures.
-      sensors.requestTemperatures();
+      wireDevices.requestTemperatures();
       state = STATE_WAITING_FOR_TEMPERATURES;
       stateBegunMs = millis();
       break;
-      
+
     default:
       Serial.print("unknown state: ");
       Serial.println(state);
@@ -261,8 +291,8 @@ void loop() {
 
 /*
    Prints the 1-wire address of each device found on the 1-wire bus.
-   The print is in a form that can be copied and pasted into an array
-   declaration.
+   The output is in a form that can be copied and pasted into an array
+   initializer.
 
    Returns the number of devices on the bus if successful,
    0 if there is an error.
@@ -270,13 +300,13 @@ void loop() {
 uint8_t print1WireAddresses() {
   uint8_t numDevices;
 
-  numDevices = sensors.getDeviceCount();
+  numDevices = wireDevices.getDeviceCount();
   Serial.print(numDevices);
   Serial.println(" temperature sensors responded:");
 
   DeviceAddress address;
   for (int i = 0; i < numDevices; ++i) {
-    if (!sensors.getAddress(address, i)) {
+    if (!wireDevices.getAddress(address, i)) {
       Serial.print("Sensor[");
       Serial.print(i);
       Serial.println("] was not found.");
@@ -312,26 +342,61 @@ void print1WireAddress(DeviceAddress addr) {
 /*
    Connect to the local WiFi Access Point.
    The Auto Reconnect feature of the ESP8266 should keep us connected.
-*/
-void connectToAccessPoint() {
-  Serial.print(F("Connecting to "));
-  Serial.println(wifiSsid);
 
-  WiFi.begin(wifiSsid, wifiPassword);
+   ssid = the WiFi Access Point to connect to.
+   pass = the password for that Access Point.
+   timeoutMs = maximum time (milliseconds) to wait for the connection.
+
+   Returns true if successful; false if a nonrecoverable error occurred.
+*/
+boolean connectToAccessPoint(char *ssid, char *pass, long timeoutMs) {
+  unsigned long startTimeMs;  // time (millis()) when we started connecting.
+  unsigned long now;  // the current time (millis())
+
+  Serial.print(F("Connecting to "));
+  Serial.println(ssid);
+
+  startTimeMs = millis();
+  WiFi.begin(ssid, wifiPassword);
+  
   // Wait for the connection or timeout.
   int wifiStatus = WiFi.status();
   while (wifiStatus != WL_CONNECTED)
   {
-    delay(100);
-    Serial.print(".");
+    now = millis();
 
+    if (wifiStatus == WL_NO_SSID_AVAIL) {
+      // AP is offline.
+      Serial.print(F("Unable to find "));
+      Serial.println(ssid);
+      return false;
+    }
+    if (wifiStatus == WL_CONNECT_FAILED) {
+      // bad password
+      Serial.print(F("Password incorrect for "));
+      Serial.println(ssid);
+      return false;
+    }
+    if ((long) (now - startTimeMs) > timeoutMs) {
+      // Connection has taken too long
+      Serial.print(F("Timeout connecting to "));
+      Serial.println(ssid);
+      return false;
+    }
+
+    /*
+       The WiFi stack is still trying to connect.
+       Give it a moment and check again.
+    */
+
+    delay(100);
     wifiStatus = WiFi.status();
   }
 
-  Serial.println();
-
   Serial.print(F("Connected, IP address: "));
   Serial.println(WiFi.localIP());
+
+  return true;
 }
 
 /********************************
@@ -411,6 +476,4 @@ char *readEEPROMString(int baseAddress, int stringNumber) {
   result[i] = '\0';
 
   return result;
-
 }
-
